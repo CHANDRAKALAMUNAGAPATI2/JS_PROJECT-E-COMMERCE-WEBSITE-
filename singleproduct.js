ff121a178a45576e5612a2a878a32d7adcb1bfda
CHANDRAKALA <121846437+CHANDRAKALAMUNAGAPATI2@users.noreply.github.com>
2026-05-15 23:32:00 +0530

    Update singleproduct.js

--- singleproduct.js
+++ singleproduct.js
@@ -1,181 +1,121 @@
-let bg = document.getElementById('bg')
+let productBg = document.getElementById('product-bg')
 let cartLength = document.getElementById('cartLength')
 let logoutBtn = document.getElementById('logoutBtn')
 let currentuser = localStorage.getItem('login_user')
+
+function list() {
+    let data = JSON.parse(localStorage.getItem("favlist"))
+    if (data == null) {
+        return []
+    } else {
+        return data
+    }
+}
+
+let favlist = list()
+// let favlist=[]
 let filteredData = []
 
 
-//navigate to login page if there is no user logged in
-if(currentuser==null){
-            location.replace('login.html')
-}
-
-//for finding the cart length
-let data = localStorage.getItem('cart')
+//for find the cart length
 function getCartDetails() {
+    let data = localStorage.getItem('cart')
     if (data == null) {
         return []
     }
     else {
         let cartData = JSON.parse(data)
         filteredData = cartData.filter(i => i.userId == currentuser)
-        return filteredData
+        return cartData
     }
 }
 let cart = getCartDetails()
-let count = filteredData.length
+count = filteredData.length
 cartLength.textContent = count
 
 
-//for create product card
-function createProductCard(products) {
-    for (let product of products) {
+//for create product interface
+function singleProductInterface(product) {
+    let img = document.createElement('img')
+    img.src = product.thumbnail
 
-        let div = document.createElement('div')
-        div.setAttribute('id', 'card')
-        div.style.backgroundColor = 'white'
-        div.style.width = '250px';
-        div.style.border = '2px solid';
-        div.style.borderRadius = '10px';
-        div.style.padding = '10px';
-        div.style.marginBottom = '5px';
-        div.style.textAlign = 'center'
+    let singleinfo = document.createElement('div')
+    singleinfo.setAttribute('id', 'singleinfo')
 
-        let img = document.createElement('img')
-        img.src = product.thumbnail;
-        img.height = '150'
-        img.width = '200'
+    let h2 = document.createElement('h2')
+    h2.textContent = product.title
 
-        let h2 = document.createElement('h2')
-        h2.textContent = product.title
-        h2.style.color = 'brown'
-        h2.style.marginBottom = '5px';
+    let p = document.createElement('p')
+    p.textContent = product.description
 
-        let h3 = document.createElement('h3')
-        h3.textContent = 'Price :$' + product.price
-        h3.style.color = 'chocolate';
-        h3.style.marginBottom = '7px'
+    let h3 = document.createElement('h3')
+    h3.textContent = 'Price :$' + product.price
 
-
-        let h4 = document.createElement('h4')
-        h4.textContent = 'Rating :'
-        for (let i = 1; i <= product.rating; i++) {
-            h4.innerText += '★'
-        }
-        if (product.rating % 1 != 0) {
-            h4.innerText += '⯨'
-        }
-        h4.style.color = 'rgb(112, 18, 62)'
-
-
-        let button = document.createElement('button')
-        button.setAttribute('id', 'cart')
-        button.textContent = '🛒'
-
-        
-        button.onclick = function() {
-            product.userId = currentuser
-
-            let cart = JSON.parse(localStorage.getItem('cart'))
-            let isExist = false
-            let newcart = cart.map(item => {
-                if (item.id == product.id && item.userId == currentuser) {
-                    item.frequency += 1
-                    isExist = true
-                }
-                return item
-            })
-
-            if (isExist) {
-                localStorage.setItem('cart', JSON.stringify(newcart))
-
-            }
-            else {
-                product.frequency = 1
-                let cart = JSON.parse(localStorage.getItem('cart'))
-                cart.push(product)
-                localStorage.setItem('cart', JSON.stringify(cart))
-                count++
-                cartLength.textContent = count
-            }
-        }
-
-        let fav = document.createElement('button')
-        fav.textContent = '🤍'
-        fav.setAttribute('id', 'fav')
-        fav.style.fontSize = '30px'
-        fav.style.width = '100px'
-        fav.style.padding='0px'
-
-        fav.onclick = () => {
-            let localfavlist = JSON.parse(localStorage.getItem("favlist"))
-            function getfavItems(){
-                if (localfavlist==null){
-                    return []
-                }
-                else{
-                    let userfavlist = localfavlist.filter((item) => {
-                if (item.liked == localStorage.getItem('login_user')) {
-                    return item
-                    }
-                })
-                return userfavlist
-            
-                }
-            }
-
-            let favlist=getfavItems()
-
-
-            let filteredData = favlist.filter((item) => {
-                return !(item.id == product.id && item.liked == currentuser)
-
-
-            })
-
-
-            localStorage.setItem("favlist", JSON.stringify(filteredData))
-
-            if (filteredData.length == 0) {
-                bg.textContent = ''
-                let h1 = document.createElement('h1')
-                h1.textContent = 'Your Wishlist is Empty 💔'
-                h1.setAttribute('id', 'empty-msg')
-                bg.appendChild(h1)
-            }
-            else {
-
-                location.reload()
-
-            }
-
-        }
-        let features = document.createElement('div')
-        features.append(button, fav)
-        features.setAttribute('id', 'features')
-        div.append(img, h2, h3, h4, features)
-        bg.append(div)
+    let h4 = document.createElement('h4')
+    h4.textContent = 'Rating :'
+    for (let i = 1; i <= product.rating; i++) {
+        h4.innerText += '★'
+    }
+    if (product.rating % 1 != 0) {
+        h4.innerText += '⯨'
     }
 
-}
+
+    let button = document.createElement('button')
+    button.setAttribute('class', 'btn btn-success m-2 p-2')
+    button.textContent = 'Add to cart'
+
+    button.onclick = function () {
+        product.userId = currentuser
+
+        let cart = JSON.parse(localStorage.getItem('cart'))
+        let isExist = false
+        let newcart = cart.map(item => {
+            if (item.id == product.id && item.userId == currentuser) {
+                item.frequency += 1
+                isExist = true
+            }
+            return item
+        })
+
+        if (isExist) {
+            localStorage.setItem('cart', JSON.stringify(newcart))
+            location.replace('index.html')
 
 
-//toggle
-function toggleMenu(){
-    document.getElementById("nav-links").classList.toggle("active");
-}
+
+        }
+        else {
+            product.frequency = 1
+            let cart = JSON.parse(localStorage.getItem('cart'))
+            cart.push(product)
+            localStorage.setItem('cart', JSON.stringify(cart))
+            count++
+            cartLength.textContent = count
+            location.replace('index.html')
+
+        }
+
+    }
 
 
-//for getting users fav items
+    //fav list 
+
+    let fav = document.createElement('p');
+    fav.textContent = '🤍'
+    fav.style.fontSize = '40px'
+
+//for getting users fav list
+
 let localfavlist = JSON.parse(localStorage.getItem("favlist"))
 function getfavItems(){
     if (localfavlist==null){
         return []
     }
     else{
-        let userfavlist = localfavlist.filter((item) => {
-            if (item.liked == localStorage.getItem('login_user')) {
-                return item
+        let userfavlist = favlist.filter((item) => {
+    if (item.liked == localStorage.getItem('login_user')) {
+        return item
         }
     })
     return userfavlist
@@ -183,26 +123,51 @@ function getfavItems(){
     }
 }
 
-let favlist=getfavItems()
-if (favlist.length == 0) {
-                bg.textContent = ''
-                let h1 = document.createElement('h1')
-                h1.textContent = 'Your Wishlist is Empty 💔'
-                h1.setAttribute('id', 'empty-msg')
-                bg.appendChild(h1)
-
-            }
-           else{
-
-               createProductCard(favlist)
-           }
+let newfavlist=getfavItems()
 
 
+    fav.onclick = () => {
+        let a = newfavlist.find((item) => {
+            return item.id == product.id
+        })
+        if (a == undefined) {
+            product['liked'] = currentuser
+            favlist.push(product)
+            localStorage.setItem('favlist', JSON.stringify(favlist))
+            location.replace('index.html')
+
+        }
+        else {
+            alert('Item already in wishlist')
+            location.replace('index.html')
+        }
+    }
+
+    singleinfo.append(h2, p, h3, h4, button, fav)
+    productBg.append(img, singleinfo)
+}
+
+//getting single product from api
+async function getSingleProduct() {
+    try {
+        if (currentuser == null) {
+            location.replace('login.html')
+        }
+        let id = localStorage.getItem('productId')
+        let res = await fetch(`https://dummyjson.com/products/${id}`)
+        let product = await res.json()
+        singleProductInterface(product)
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+getSingleProduct()
 
 
-//Logout functionality
+
+//logout button
 logoutBtn.onclick = function () {
     localStorage.removeItem('login_user')
     location.replace('login.html')
 }
-
