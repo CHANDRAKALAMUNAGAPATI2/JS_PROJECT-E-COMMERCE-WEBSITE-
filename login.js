00aa86377b41f15cf1440817892ff55a7145b16c
CHANDRAKALA <121846437+CHANDRAKALAMUNAGAPATI2@users.noreply.github.com>
2026-05-17 20:57:57 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,47 +1,59 @@
 let email=document.getElementById('email')
+
 let password=document.getElementById('password')
 let msg=document.getElementById('msg')
-let btn=document.getElementById('btn')
-
-let user=localStorage.getItem('login_user')
-    if(user!=null){
-      location.replace('index.html')
-
-    }
-
-async function login(){
-
-  let res=await fetch('http://localhost:4000/users?email='+email.value)
-  let jsonRes=await res.json()
-  if(jsonRes.length==0){
-    msg.textContent='User not Exist'
-    msg.style.color='red'
-    return
-  }
-  else{
-    if(jsonRes[0].password==password.value){
-        localStorage.setItem('login_user',email.value)
-        location.replace('home.html')
+console.log('hi');
+console.log(login);
+let a=document.getElementById('login')
 
 
-    }
-    else{
-        msg.textContent='Invalid Credentials'
-        msg.style.color='red'
-        return
-    }    
-
-  }
-  
-}
-
-btn.onclick=function(event){
-    event.preventDefault()
-    if(password.value=='' || email.value==''){
-        alert("Please fill all fields")
-        return
-    }
+        a.onclick=(e)=>{
+            e.preventDefault()
+            if(email.value.trim()==='' || password.value.trim()===''){
     
-    login()
+                alert('please fill all fields')
+                return
+    
+            }
+            let data=JSON.parse(localStorage.getItem('usersdata'))
+            function getusersdata(){
+                if(data==null){
+                    return []
+                }
+                else{
+                    return data
+                }
+            }
 
-}
+            let usersdata=getusersdata()
+
+            let isalreadyExist=false
+
+            for(let singleuser of usersdata){
+                if(email.value==singleuser.email){
+                    isalreadyExist=true
+                    if(password.value==singleuser.password){
+                        localStorage.setItem('login_user',email.value)
+                        location.replace('index.html')
+
+                    }
+                    else{
+                        msg.textContent='Invalid Credentials'
+                        msg.style.color='red'
+                        return
+                    }
+                                               
+                }
+            }
+            if(isalreadyExist==false){
+                msg.textContent="User doesn't exist"
+                msg.style.color='red'
+
+            }
+
+                
+
+        
+    }
+
+// localStorage.removeItem('usersdata')
